7b35170600aad7c9efa0e44183afd18d177dd682
Assembly <assembly0x7c00@gmail.com>
2021-08-18 11:22:50 +0800

    server init

---
+++ server.ts
@@ -0,0 +1,30 @@
+/**
+ * 这是一个服务端程序
+ */
+class UdpPunchServer
+{
+	private config: UdpPunchServerConfig;
+	constructor(config: UdpPunchServerConfig){
+		this.config = config;
+	}
+
+	/**
+	 * 运行服务器
+	 */
+
+	public run() {
+		console.log("Hello server");
+	}
+};
+
+interface UdpPunchServerConfig {
+	localPort: 10000,
+	localHost: "localhsot"
+};
+
+let config: UdpPunchServerConfig = {
+	localPort: 10000,
+	localHost: "localhost"
+};
+
+(new UdpPunchServer(config)).run();
